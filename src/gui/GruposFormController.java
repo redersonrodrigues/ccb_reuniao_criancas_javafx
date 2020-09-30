fc5718f41d88c17dfccae2ff3509ad124ccdab04
Rederson Rodrigues <r.ataliba@hotmail.com>
2020-09-30 14:18:15 -0300

    Validação de excessões

--- src/gui/GruposFormController.java
+++ src/gui/GruposFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Grupos;
+import model.exceptions.ValidationException;
 import model.services.GruposService;
 
 public class GruposFormController implements Initializable{
@@ -85,10 +88,13 @@ public class GruposFormController implements Initializable{
 		notifyDataChangeListeners();
 		Utils.currentStage(event).close();
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 		catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
 		}
-		
+
 	}
 	
 	// mandar envento para todos os listeners
@@ -105,10 +111,25 @@ public class GruposFormController implements Initializable{
 	private Grupos getFormData() {
 		
 		Grupos obj = new Grupos();
+		
+		ValidationException exception = new ValidationException("Validation error");
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if (txtNome.getText() == null || txtNome.getText().trim().equals("")) {
+		
+			exception.addError("nome", "O campo não pode ser vazio!");
+		
+		}
+		
 		obj.setNome(txtNome.getText());
 		
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
 		return obj;
+		
 	}
 
 
@@ -145,5 +166,19 @@ public class GruposFormController implements Initializable{
 		
 		
 	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		
+		Set<String> fields = errors.keySet();
+		
+		if (fields.contains("nome")) {
+			
+			labelErrorNome.setText(errors.get("nome"));
+			
+			
+		}
+		
+	}
+	
 
 }
